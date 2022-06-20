2e5afeb56360cbd66fa3591826383218219ac9a3
Web <web@it.step>
2022-06-20 19:00:34 +0300

    added-function-to-write-space-for-letters

--- utilits.js
+++ utilits.js
@@ -1,7 +1,16 @@
+import {lettersField} from './constants.js';
 function getUserLetter(){
     const userLetter = document.getElementById("userLetterField").value;
     return userLetter;
 }
 function splitWordToLetters(wordToSplit){
     return wordToSplit.split('');
+}
+function createSpaceForLetters(word){
+    for(let i=0; i < word.length;i++){
+        let letterWrapper = document.createElement('div');
+        letterWrapper.className = `letterWrapper ${word[i]}`;
+        letterWrapper.textContent = '_';
+        lettersField.append(letterWrapper);
+    }
 }
