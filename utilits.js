43b02d1141e7bdcbff2636822ed83f6d21b4adc1
Web <web@it.step>
2022-06-20 18:37:00 +0300

    function-to-split-words-to-letters

--- utilits.js
+++ utilits.js
@@ -2,3 +2,6 @@ function getUserLetter(){
     const userLetter = document.getElementById("userLetterField").value;
     return userLetter;
 }
+function splitWordToLetters(wordToSplit){
+    return wordToSplit.split('');
+}
