1bb7a7c09e90e3ee7a12dcba024dfe3c00eb0848
Web <web@it.step>
2022-06-20 19:43:04 +0300

    added-function-for-creating-space-for-each-word

--- domcomponents.js
+++ domcomponents.js
@@ -1,8 +1,6 @@
-import {lettersField} from './constants.js';
 export function createOneLetterSpace(lettersField){
     const letterSpace = document.createElement('div');
     letterSpace.className = "unknownLetter";
     letterSpace.textContent = "_";
     lettersField.append(letterSpace);
 }
-createOneLetterSpace(lettersField);
--- utilits.js
+++ utilits.js
@@ -1,8 +1,14 @@
 import {lettersField} from './constants.js';
+import {createOneLetterSpace} from './domcomponents.js';
 function getUserLetter(){
     const userLetter = document.getElementById("userLetterField").value;
     return userLetter;
 }
 function splitWordToLetters(wordToSplit){
     return wordToSplit.split('');
+}
+function writeSpacesForLetters(word){
+    for(let i=0;i<word.length;i++){
+        createOneLetterSpace(lettersField);
+    }
 }
