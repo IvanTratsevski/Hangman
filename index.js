6cbc223394a8a82ced1b768909facfc91b52d8d0
Web <web@it.step>
2022-06-24 20:38:27 +0300

    added-game-iteration-logic

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-import {getRandomWord, splitWordToLetters, writeSpacesForLetters} from './utilits.js';
-import {words,submitButton} from './constants.js';
-import {gameIteration} from './handlers.js'
+import {getRandomWord, splitWordToLetters, writeSpacesForLetters,getUserLetter,getPostitionOfTrueLetters,showTrueLetters, changeImageOfMan,isVictory} from './utilits.js';
+import {words,submitButton,images} from './constants.js';
 function startTheGame() {
    let deathScore = 0;
    const secretWord = getRandomWord(words);
    const lettersOfSecretWord = splitWordToLetters(secretWord);
    writeSpacesForLetters(lettersOfSecretWord);
-   submitButton.addEventListener("click",gameIteration(deathScore,lettersOfSecretWord));
+   submitButton.onclick = function() {
+      let userLetter = getUserLetter();
+      if(getPostitionOfTrueLetters(lettersOfSecretWord,userLetter).length !==0){
+         showTrueLetters(getPostitionOfTrueLetters(lettersOfSecretWord,userLetter),userLetter);
+      } else {
+         console.log('FFF');
+         deathScore++;
+         changeImageOfMan(deathScore,images);
+      }
+      if(deathScore === 3){
+         alert("proigrish");
+      } else if (isVictory()){
+         alert("pobeda");
+      }
+   };
 }
 startTheGame();
